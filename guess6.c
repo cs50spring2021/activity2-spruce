900e4e9718a3e090c34daa70c6813c976143b56a
So Amano <f003wv6@plank.thayer.dartmouth.edu>
2021-04-07 09:51:29 -0400

    Added limit of 10 guesses

--- guess6.c
+++ guess6.c
@@ -29,7 +29,8 @@ main(const int argc, const char * argv[])
   int answer; // the secret number to guess
   int guess;  // the user's guess
   int max;    // we'll pick a number in [1..max]
-
+  int counter=1;
+  
   // interpret arguments
   if (argc == 1) {
     // no arguments (other than the command) - use default value
@@ -51,7 +52,7 @@ main(const int argc, const char * argv[])
   printf("I'm thinking of a number between 1-%d.\n", max);
   guess = askGuess(1, max);
 
-  while (guess != 0 && guess != answer) {
+  while (guess != 0 && guess != answer && counter < 10) {
     if (guess < 0) {
       printf("invalid number!\n");
     } else if (guess > answer) {
@@ -60,6 +61,7 @@ main(const int argc, const char * argv[])
       printf("too low!\n");
     }
     guess = askGuess(1, max);
+    counter = counter + 1;
   }
 
   if (guess == answer)
